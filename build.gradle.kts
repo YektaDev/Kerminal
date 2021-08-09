03b9d72e51c0c4426b7e50d0653f5d90d29c36b4
Ali Khaleqi Yekta <Me@Yekta.Dev>
2021-08-09 11:05:06 +0430

    Add Compose Desktop

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,23 +1,30 @@
+@file:Suppress("SpellCheckingInspection")
+
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     kotlin("jvm") version "1.5.21"
-    application
+    id("org.jetbrains.compose") version "1.0.0-alpha1"
+}
+
+repositories {
+    mavenCentral()
+    maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+    google()
 }
 
 group = "dev.yekta"
 version = "1.0.0"
 
-repositories {
-    mavenCentral()
-}
-
-application {
-    mainClass.set("MainKt")
-}
-
 dependencies {
+    implementation(compose.desktop.currentOs)
     implementation("com.github.ajalt.clikt:clikt:3.2.0")
+}
+
+compose.desktop {
+    application {
+        mainClass = "MainKt"
+    }
 }
 
 tasks.withType<KotlinCompile> {
--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,3 +1,47 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+import androidx.compose.desktop.DesktopMaterialTheme
+import androidx.compose.desktop.ui.tooling.preview.Preview
+import androidx.compose.material.Button
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.window.Window
+import androidx.compose.ui.window.application
+import com.github.ajalt.clikt.core.CliktCommand
+import com.github.ajalt.clikt.parameters.options.default
+import com.github.ajalt.clikt.parameters.options.option
+import com.github.ajalt.clikt.parameters.options.prompt
+import com.github.ajalt.clikt.parameters.types.int
+
+class Hello : CliktCommand() {
+    val count: Int by option(help="Number of greetings").int().default(1)
+    val name: String by option(help="The person to greet").prompt("Your name")
+
+    override fun run() {
+        repeat(count) {
+            echo("Hello $name!")
+        }
+    }
+}
+
+@Composable
+@Preview
+fun App() {
+    var text by remember { mutableStateOf("Hello, World!") }
+
+    DesktopMaterialTheme {
+        Button(onClick = {
+            text = "Hello, Desktop!"
+        }) {
+            Text(text)
+        }
+    }
+}
+
+fun main() = application {
+    Window(onCloseRequest = ::exitApplication) {
+        App()
+    }
 }
