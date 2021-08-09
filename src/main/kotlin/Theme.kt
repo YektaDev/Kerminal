3907deebafcaaca074a3c94f354adcda132d335f
Ali Khaleqi Yekta <Me@Yekta.Dev>
2021-08-09 23:04:50 +0430

    Add BitaTheme

---
+++ src/main/kotlin/Theme.kt
@@ -0,0 +1,32 @@
+import androidx.compose.desktop.DesktopMaterialTheme
+import androidx.compose.material.Colors
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+
+@Composable
+fun BitaTheme(themeConfig: Theme, content: @Composable () -> Unit) {
+    DesktopMaterialTheme(
+        colors = Colors(
+            isLight = !themeConfig.isDark,
+
+            primary = Color(themeConfig.color.primary),
+            primaryVariant = Color(themeConfig.color.primaryVariant),
+
+            secondary = Color(themeConfig.color.secondary),
+            secondaryVariant = Color(themeConfig.color.secondary),
+
+            onSurface = Color(themeConfig.color.front),
+            onBackground = Color(themeConfig.color.front),
+            onPrimary = Color(themeConfig.color.front),
+            onSecondary = Color(themeConfig.color.front),
+            onError = Color(themeConfig.color.front),
+
+            background = Color(themeConfig.color.back),
+            surface = Color(themeConfig.color.back),
+
+            error = Color(themeConfig.color.error),
+        )
+    ) {
+        content()
+    }
+}
