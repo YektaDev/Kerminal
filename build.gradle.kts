926c4dbf4f78a589c034319609e23b1f04b3e3ec
Ali Khaleqi Yekta <Me@Yekta.Dev>
2021-08-10 15:09:04 +0430

    Add & configure ShadowJar

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 @file:Suppress("SpellCheckingInspection")
 
+import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 val appGroup = "dev.yekta"
@@ -8,6 +9,7 @@ val appVersion = "1.0.0"
 plugins {
     kotlin("jvm") version "1.5.21"
     id("org.jetbrains.compose") version "1.0.0-alpha1"
+    id("com.github.johnrengelman.shadow") version "7.0.0"
 }
 
 repositories {
@@ -46,4 +48,24 @@ tasks.processResources {
 
 tasks.withType<KotlinCompile> {
     kotlinOptions.jvmTarget = "13"
+}
+
+val jar by tasks.getting(Jar::class) {
+    manifest {
+        attributes["Manifest-Version"] = "1.0"
+        attributes["Main-Class"] = "Main"
+    }
+
+    // To add all of the dependencies
+    duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+    from(sourceSets.main.get().output)
+    dependsOn(configurations.runtimeClasspath)
+    from({
+        configurations.runtimeClasspath.get().filter { it.name.endsWith("jar") }.map { zipTree(it) }
+    })
+}
+
+// Output to build/libs/Bita.jar
+tasks.withType<ShadowJar> {
+    archiveFileName.set("Bita.jar")
 }
