6f056627ebe89f762b43a61e78e7d2d8df8ab676
Ali Khaleqi Yekta <Me@Yekta.Dev>
2021-08-10 09:06:16 +0430

    Add Settings.kt

---
+++ src/main/kotlin/Settings.kt
@@ -0,0 +1,31 @@
+import java.net.URL
+import java.util.*
+
+object Resource {
+    private const val iconPath = "icon.svg"
+    private const val propertiesPath = "app.properties"
+
+    val properties: Properties? by lazy {
+        val stream = Thread.currentThread().contextClassLoader.getResourceAsStream(propertiesPath)
+        val props = Properties()
+        props.load(stream)
+        props
+    }
+    val icon: URL? by lazy {
+        get(iconPath)
+    }
+
+    fun get(path: String): URL? = Resource::class.java.getResource(path) ?: null
+}
+
+object App {
+    val name: String by lazy {
+        Resource.properties?.getProperty("name") ?: "[UNRESOLVED_NAME]"
+    }
+    val version: String by lazy {
+        Resource.properties?.getProperty("version") ?: "[UNRESOLVED_VERSION]"
+    }
+    val group: String by lazy {
+        Resource.properties?.getProperty("group") ?: "[UNRESOLVED_GROUP]"
+    }
+}
