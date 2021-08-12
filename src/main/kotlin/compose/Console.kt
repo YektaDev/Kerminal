90068ee26a1e6f89ab9211c458266a2b5c554e0c
Ali Khaleqi Yekta <Me@Yekta.Dev>
2021-08-12 12:18:04 +0430

    Fix editable lines bug

--- src/main/kotlin/compose/Console.kt
+++ src/main/kotlin/compose/Console.kt
@@ -9,15 +9,11 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.input.OffsetMapping
 import androidx.compose.ui.text.input.TransformedText
-import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.unit.em
 import androidx.compose.ui.unit.sp
 
@@ -29,6 +25,7 @@ fun Console() {
         modifier = Modifier.fillMaxSize(),
         value = text,
         singleLine = false,
+        maxLines = Int.MAX_VALUE,
         textStyle = TextStyle(
             fontFamily = FontFamily.Monospace,
             fontStyle = FontStyle.Normal,
@@ -42,18 +39,7 @@ fun Console() {
             )
         },
         onValueChange = {
-            text = buildAnnotatedString {
-                withStyle(style = SpanStyle(color = Color.Blue)) {
-                    append(it.split("ccc")[0])
-                }
-
-                withStyle(style = SpanStyle(color = Color.Red)) {
-                    it.split("ccc").forEachIndexed { index, s ->
-                        if (index != 0)
-                            this.append(s)
-                    }
-                }
-            }.toString()
+            text = processInput(it).toString()
         }
     )
 }
