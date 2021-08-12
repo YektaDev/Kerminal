757ca2b39b78624e6e76df6ead302fa5dd91a22a
Ali Khaleqi Yekta <Me@Yekta.Dev>
2021-08-12 11:53:55 +0430

    Update MainPage

--- src/main/kotlin/page/MainPage.kt
+++ src/main/kotlin/page/MainPage.kt
@@ -2,23 +2,24 @@ package page
 
 import Theme
 import androidx.compose.desktop.ui.tooling.preview.Preview
-import androidx.compose.material.Button
-import androidx.compose.material.Text
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import compose.BitaTheme
+import compose.Console
 
-@Composable
 @Preview
+@Composable
 fun MainPage(themeData: Theme) = BitaTheme(themeData) {
-    var text by remember { mutableStateOf("Hello, World!") }
-
-    Button(onClick = {
-        text = "Hello, Desktop!"
-    }) {
-        Text(text)
+    Surface(
+        modifier = Modifier
+            .background(Color(themeData.color.back))
+            .fillMaxSize(),
+        contentColor = Color(themeData.color.front)
+    ) {
+        Console()
     }
 }
