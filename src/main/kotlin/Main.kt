7a0a2783852649b10ec481a6123d5d5d808c419f
Ali Khaleqi Yekta <Me@Yekta.Dev>
2021-08-14 06:18:40 +0430

    Remove Clikt sample from Main.kt

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,26 +1,9 @@
-import com.github.ajalt.clikt.core.CliktCommand
-import com.github.ajalt.clikt.parameters.options.default
-import com.github.ajalt.clikt.parameters.options.option
-import com.github.ajalt.clikt.parameters.options.prompt
-import com.github.ajalt.clikt.parameters.types.int
 import com.sksamuel.hoplite.ConfigLoader
 import com.sksamuel.hoplite.toml.TomlPropertySource
 import compose.launchAppWindow
 import page.InitialErrorPage
 import page.MainPage
 import java.io.File
-
-// TODO: Handle the use of Clikt
-class Hello : CliktCommand() {
-    val count: Int by option(help = "Number of greetings").int().default(1)
-    val name: String by option(help = "The person to greet").prompt("Your name")
-
-    override fun run() {
-        repeat(count) {
-            echo("Hello $name!")
-        }
-    }
-}
 
 object Main {
     @JvmStatic
