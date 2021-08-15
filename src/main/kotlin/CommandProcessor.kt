2dd4a23c87b4e676699637afa9222dc3db42ad72
Ali Khaleqi Yekta <Me@Yekta.Dev>
2021-08-15 06:56:47 +0430

    Add CommandProcessor

---
+++ src/main/kotlin/CommandProcessor.kt
@@ -0,0 +1,34 @@
+import command.toBitaCommand
+
+object CommandProcessor {
+    fun run(rawInput: String) {
+        EventHandler.beforeCommand(rawInput)
+
+        val parsedCommand = rawInput.cleanCommand()
+        when (parsedCommand.size) {
+            0 -> { // Ignored
+            }
+            1 -> {
+                runCommand(parsedCommand[0])
+            }
+            else -> {
+                runCommand(parsedCommand[0], parsedCommand.copyOfRange(1, parsedCommand.size))
+            }
+        }
+
+        EventHandler.afterCommand(rawInput)
+    }
+
+    private fun String.cleanCommand(): Array<String> = this.trim()
+        .split(' ')
+        .filter { it.isNotEmpty() }
+        .toTypedArray().apply {
+            if (size >= 1) {
+                this[0] = this[0].lowercase()
+            }
+        }
+
+    private fun runCommand(command: String, args: Array<String> = arrayOf()) {
+        command.toBitaCommand()?.run(args)
+    }
+}
