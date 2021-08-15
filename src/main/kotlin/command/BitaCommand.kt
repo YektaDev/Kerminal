344a9434f4fd0c1a2bbae01d7351ac03f7663058
Ali Khaleqi Yekta <Me@Yekta.Dev>
2021-08-15 06:55:48 +0430

    Add BitaCommand

---
+++ src/main/kotlin/command/BitaCommand.kt
@@ -0,0 +1,49 @@
+package command
+
+import State
+import com.github.ajalt.clikt.core.Abort
+import com.github.ajalt.clikt.core.CliktCommand
+import com.github.ajalt.clikt.core.CliktError
+import com.github.ajalt.clikt.core.PrintCompletionMessage
+import com.github.ajalt.clikt.core.PrintHelpMessage
+import com.github.ajalt.clikt.core.PrintMessage
+import com.github.ajalt.clikt.core.ProgramResult
+import com.github.ajalt.clikt.core.UsageError
+
+sealed class BitaCommand(
+    val command: String,
+    val help: String
+) : CliktCommand(name = command, help = help) {
+    fun run(args: Array<String>): Unit = with(State.console) {
+        try {
+            parse(args)
+        } catch (e: Exception) {
+            printError(e.message)
+        } catch (e: ProgramResult) {
+        } catch (e: PrintHelpMessage) {
+            if (e.error) {
+                printError(e.command.getFormattedHelp())
+            } else {
+                printWarning(e.command.getFormattedHelp())
+            }
+        } catch (e: PrintCompletionMessage) {
+            printInfo(e.message)
+        } catch (e: PrintMessage) {
+            if (e.error) {
+                printError(e.message)
+            } else {
+                printInfo(e.message)
+            }
+        } catch (e: UsageError) {
+            printError(e.helpMessage())
+        } catch (e: CliktError) {
+            printError(e.message)
+        } catch (e: Abort) {
+            if (e.error) {
+                printError(currentContext.localization.aborted())
+            } else {
+                printInfo(currentContext.localization.aborted())
+            }
+        }
+    }
+}
